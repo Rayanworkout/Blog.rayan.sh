7dd86f615ee915c7a2c2ac5e73bf62f47e15351d
Rayan <rayan13170@protonmail.com>
2023-12-29 20:00:57 +0100

    debug false + updated script

--- backend/blog/settings.py
+++ backend/blog/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 CORS_ALLOWED_ORIGINS = [
--- pipeline.sh
+++ pipeline.sh
@@ -7,16 +7,17 @@
 # Visudo
 # www-data ALL=(ALL) NOPASSWD: /bin/rm -r /var/www/html/Blog.rayan.sh
 # www-data ALL=(ALL) NOPASSWD: /bin/rm -r /var/www/html/blog_latest
+# www-data ALL=(ALL) NOPASSWD: /usr/sbin/service apache2 restart
+# www-data ALL=(ALL) NOPASSWD: /bin/rm -r /var/www/html/blog/backend/static
 
 # Stop if an error occurs
 set -e
 
 cd /var/www/html/
-
 echo "Current pwd: $(pwd)"
 
 # First delete blog_latest and Blog.rayan.sh folders if they exists
-blog_latest="/var/www/blog_latest"
+blog_latest="/var/www/html/blog_latest"
 git_cloned="/var/www/html/Blog.rayan.sh"
 
 if [ -d "$blog_latest" ]; then
@@ -30,7 +31,7 @@ fi
 
 if [ -d "$git_cloned" ]; then
     echo "git_cloned exists. Removing..."
-    rm -r "$git_cloned"
+    sudo rm -r "$git_cloned"
     echo "git_cloned removed."
 else
     echo "git_cloned= does not exist."
@@ -47,6 +48,17 @@ mv ./Blog.rayan.sh/ ./blog_latest/
 echo "> ok"
 
 
+# I check if DEBUG = TRUE, if so I stop the build
+settings_file="blog_latest/backend/blog/settings.py"
+
+if grep -q "DEBUG = True" "$settings_file"; then
+    echo "DEBUG is set to True in Django settings. Aborting build."
+    exit 1
+else
+    echo "DEBUG is False, continuing with the build."
+fi
+
+
 # Trailing slash after source directory to copy the content of the folder
 # and not the folder itself
 echo "> Copying folder to /var/www/html"
@@ -60,7 +72,6 @@ python3 -m venv .venv
 echo '> venv ok'
 
 echo '> Activate venv and install requirements'
-
 source .venv/bin/activate
 pip install -r requirements.txt
 echo '> Requirements ok'
@@ -68,4 +79,38 @@ echo '> Requirements ok'
 echo '> Starting migrations'
 python3 ./manage.py makemigrations
 python3 ./manage.py migrate
-echo 'migrations ok'
+echo '> Migrations ok'
+
+
+
+static_folder="/var/www/html/blog/backend/static"
+
+# Check if the folder exists
+if [ -d "$static_folder" ]; then
+    echo "Static folder exists. Deleting it ..."
+    # Remove the folder and its contents
+    rm -r "$static_folder"
+    echo "Folder deleted."
+fi
+
+
+echo '> Collecting static'
+python3 ./manage.py collectstatic
+echo '> Static ok'
+
+echo '> Changing db permission'
+chmod 664 ./db.sqlite3
+echo '> ok'
+
+
+# Deleting temp folder
+echo '> Deleting temp folder'
+sudo rm -r "$blog_latest"
+echo '> Done'
+
+echo "Restarting Apache service ..."
+sudo service apache2 restart
+
+echo "Done, build successful."
+
+msg="Blog_backend_build_is_successful"
